29711ab4b5862d94eebbf708082f3d4d70a707c2
Firoziyash <154004097+Firoziyash@users.noreply.github.com>
2024-09-12 20:09:43 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const footer = `
+        <footer>&copy; Firoziyash 2024-09-12</footer>
+    `;
+    document.body.insertAdjacentHTML("beforeend", footer);
+});
